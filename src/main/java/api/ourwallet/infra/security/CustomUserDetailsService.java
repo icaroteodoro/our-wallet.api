a7af042f6a940efc2b43b60c39a50497c440615b
Ícaro Teodoro <icaro.teodoro84@icloud.com>
2024-09-05 11:40:49 -0300

    update security config

--- src/main/java/api/ourwallet/infra/security/CustomUserDetailsService.java
+++ src/main/java/api/ourwallet/infra/security/CustomUserDetailsService.java
@@ -1,4 +1,4 @@
-package api.ourwallet.infra.secutiry;
+package api.ourwallet.infra.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
